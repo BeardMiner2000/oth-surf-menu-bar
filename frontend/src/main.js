2a00846292f1493c5cb7a4921bae2b42e985a3ac
BeardMiner2000 <jl.zoeckler@gmail.com>
2026-04-26 10:06:02 -0700

    Fix packaged tray icon

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,9 +6,13 @@ const menubar = require('menubar');
 const API_URL = process.env.OTH_SURF_API_URL || 'https://oth-surf-scraper.onrender.com';
 const indexUrl = new URL(pathToFileURL(path.join(__dirname, 'index.html')).toString());
 indexUrl.searchParams.set('api', API_URL);
+const trayIconPath = process.resourcesPath
+  ? path.join(process.resourcesPath, 'trayTemplate.png')
+  : path.join(__dirname, '..', 'resources', 'trayTemplate.png');
 
 const mb = menubar({
   index: indexUrl.toString(),
+  icon: trayIconPath,
   tooltip: 'OTH SURF',
   width: 420,
   height: 640,
